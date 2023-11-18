754ac1cc307ab4df5b695d3586a51237b51a237f
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-18 13:52:33 -0800

    chore: add gpu control

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from model_setup import setup_model
 from train import train_model
 
 def main():
-    device = get_device()
     batch_size = 64
+    use_gpus = 2
+    print(f"Batch size: {batch_size}")
+    print(f"Using {use_gpus} GPUs")
+    device = get_device()
     trainloader = get_train_loader(batch_size=batch_size)
-    model = setup_model(device)
+    model = setup_model(device, use_gpus=use_gpus)
     train_model(model, trainloader, device)
 
 if __name__ == "__main__":
--- model_setup.py
+++ model_setup.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.models as models
 from torchvision.models import ResNet50_Weights
 
-def setup_model(device, num_classes=100):
+def setup_model(device, num_classes=100, use_gpus=1):
     weights = ResNet50_Weights.DEFAULT
     model = models.resnet50(weights=weights)
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
@@ -11,9 +11,8 @@ def setup_model(device, num_classes=100):
     num_gpus = torch.cuda.device_count()
     print(f"Number of GPUs available: {num_gpus}")
 
-    if num_gpus > 1:
-        print(f"Let's use {num_gpus} GPUs!")
-        model = torch.nn.DataParallel(model)
+    if num_gpus > 1 and use_gpus > 1:
+        model = torch.nn.DataParallel(model, device_ids=list(range(use_gpus)))
 
     return model
 
