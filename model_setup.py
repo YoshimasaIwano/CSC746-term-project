71692254b4b8956390509655736684ea6b5c280b
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-18 11:04:39 -0800

    chore: change the output

--- model_setup.py
+++ model_setup.py
@@ -1,5 +1,5 @@
 import torch
-from torchvision import models
+import torchvision.models as models
 from torchvision.models import ResNet50_Weights
 
 def setup_model(device, num_classes=100):
@@ -8,8 +8,12 @@ def setup_model(device, num_classes=100):
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     model.to(device)
 
-    if torch.cuda.device_count() > 1:
-        print(f"Let's use {torch.cuda.device_count()} GPUs!")
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
+
+    if num_gpus > 1:
+        print(f"Let's use {num_gpus} GPUs!")
         model = torch.nn.DataParallel(model)
 
     return model
+
--- train.py
+++ train.py
@@ -8,12 +8,12 @@ def train_model(model, trainloader, device, epochs=1):
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     print("Starting training...")
-    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, with_stack=True) as prof:
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, group_by_input_shape=True) as prof:
         for epoch in range(epochs): 
-            print(f"Starting epoch {epoch + 1}/{epochs}")
+            # print(f"Starting epoch {epoch + 1}/{epochs}")
             running_loss = 0.0
             for i, data in enumerate(trainloader, 0):
-                print(f"  Training batch {i + 1}")
+                # print(f"  Training batch {i + 1}")
                 inputs, labels = data[0].to(device), data[1].to(device)
 
                 optimizer.zero_grad()
@@ -23,13 +23,13 @@ def train_model(model, trainloader, device, epochs=1):
                 optimizer.step()
 
                 running_loss += loss.item()
-                if i % 2000 == 1999: 
-                    print(f'  [{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                    running_loss = 0.0
+                # if i % 2000 == 1999: 
+                #     # print(f'  [{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                #     running_loss = 0.0
 
-            print(f"Completed epoch {epoch + 1}/{epochs}")
+            # print(f"Completed epoch {epoch + 1}/{epochs}")
 
     print("Training complete. Printing profiler results...")
-    print(prof.key_averages(group_by_stack_n=5).table(sort_by="cuda_time_total", row_limit=10))
+    print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=5))
 
 
