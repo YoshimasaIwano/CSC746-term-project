8e2693ef02f1c4474f4fb43485de3e9ce6daa8cc
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-19 01:12:25 -0800

    chore: add tensor board

--- tensorflow/train.py
+++ tensorflow/train.py
@@ -1,27 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
 
-# Callback for TensorFlow Profiler
-class ProfilerCallback(tf.keras.callbacks.Callback):
-    def __init__(self, log_dir, profile_batch):
-        super().__init__()
-        self.log_dir = log_dir
-        self.profile_batch = profile_batch
+def train_model(model, trainloader, epochs=1, log_dir='log'):
+    # Callback for TensorBoard
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, profile_batch='1,10')
 
-    def on_batch_end(self, batch, logs=None):
-        if batch == self.profile_batch:
-            tf.profiler.experimental.stop()
-            tf.profiler.experimental.start(self.log_dir)
-
-def train_model(model, trainloader, epochs=1):
-    log_dir = 'log'  # Specify the directory to save profiling logs
-    profile_batch = 2  # Specify which batch to profile
-
-    callbacks = [ProfilerCallback(log_dir, profile_batch)]
-
-    # Start the TensorFlow profiler
-    tf.profiler.experimental.start(log_dir)
-
-    model.fit(trainloader, epochs=epochs, callbacks=callbacks)
-
-    # Stop the TensorFlow profiler after training
-    tf.profiler.experimental.stop()
+    # Training the model
+    model.fit(trainloader, epochs=epochs, callbacks=[tensorboard_callback])
