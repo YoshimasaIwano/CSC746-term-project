f5d8599ced3c2d0259457a0b1ee086521227c9d9
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-19 00:54:08 -0800

    chore: add log profiler

--- tensorflow/train.py
+++ tensorflow/train.py
@@ -1,2 +1,27 @@
+import tensorflow as tf
+
+# Callback for TensorFlow Profiler
+class ProfilerCallback(tf.keras.callbacks.Callback):
+    def __init__(self, log_dir, profile_batch):
+        super().__init__()
+        self.log_dir = log_dir
+        self.profile_batch = profile_batch
+
+    def on_batch_end(self, batch, logs=None):
+        if batch == self.profile_batch:
+            tf.profiler.experimental.stop()
+            tf.profiler.experimental.start(self.log_dir)
+
 def train_model(model, trainloader, epochs=1):
-    model.fit(trainloader, epochs=epochs)
+    log_dir = 'log'  # Specify the directory to save profiling logs
+    profile_batch = 2  # Specify which batch to profile
+
+    callbacks = [ProfilerCallback(log_dir, profile_batch)]
+
+    # Start the TensorFlow profiler
+    tf.profiler.experimental.start(log_dir)
+
+    model.fit(trainloader, epochs=epochs, callbacks=callbacks)
+
+    # Stop the TensorFlow profiler after training
+    tf.profiler.experimental.stop()
