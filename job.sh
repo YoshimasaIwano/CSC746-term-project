fc620b78b1169cfac384b737178273f727dd516b
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-18 16:20:02 -0800

    chore: only run gpu 1

--- job.sh
+++ job.sh
@@ -5,7 +5,7 @@ for batch_size in 64 128 512 1024
 do
     for gpu_count in 1 2 4
     do
-        for data_size_factor in 10 2 1
+        for data_size_factor in 10
         do
             echo "Running with batch size $batch_size, $gpu_count GPUs, and data size factor $data_size_factor"
             python main.py --batch_size $batch_size --use_gpus $gpu_count --data_size_factor $data_size_factor
