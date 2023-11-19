93311eb8ed043ac783e0e8ed24379fa6f930f16a
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-19 00:48:42 -0800

    fix: multiple gpu error

--- tensorflow/model_setup.py
+++ tensorflow/model_setup.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
 
 def setup_model(use_gpus=1):
-    strategy = tf.distribute.MirroredStrategy() if use_gpus > 1 else None
-    with strategy.scope() if strategy else dummy_context_mgr():
+    if use_gpus > 1:
+        strategy = tf.distribute.MirroredStrategy()
+        with strategy.scope():
+            model = tf.keras.applications.ResNet50(input_shape=(32, 32, 3), weights=None, classes=100)
+            # Using SGD optimizer
+            sgd_optimizer = tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)
+            model.compile(optimizer=sgd_optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    else:
         model = tf.keras.applications.ResNet50(input_shape=(32, 32, 3), weights=None, classes=100)
-        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        # Using SGD optimizer
+        sgd_optimizer = tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)
+        model.compile(optimizer=sgd_optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
     return model
