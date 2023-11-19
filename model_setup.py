d69ebfa496470561fce9aef014614515288226af
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-11-18 23:53:51 -0800

    chore: use resnet152

--- model_setup.py
+++ model_setup.py
@@ -1,8 +1,11 @@
 import torch
-from model import CustomCNN
+import torchvision.models as models
+from torchvision.models import ResNet152_Weights
 
 def setup_model(device, num_classes=100, use_gpus=1):
-    model = CustomCNN()
+    weights = ResNet152_Weights.DEFAULT
+    model = models.resnet152(weights=weights)
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     model.to(device)
 
     num_gpus = torch.cuda.device_count()
@@ -13,3 +16,17 @@ def setup_model(device, num_classes=100, use_gpus=1):
 
     return model
 
+# import torch
+# from model import CustomCNN
+
+# def setup_model(device, num_classes=100, use_gpus=1):
+#     model = CustomCNN()
+#     model.to(device)
+
+#     num_gpus = torch.cuda.device_count()
+#     print(f"Number of GPUs available: {num_gpus}")
+
+#     if num_gpus > 1 and use_gpus > 1:
+#         model = torch.nn.DataParallel(model, device_ids=list(range(use_gpus)))
+
+#     return model
